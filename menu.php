e2d2a9b2c67156a58c2999ab186d132d336ed7e4
fernando <fernando@orion>
2020-08-04 14:45:43 -0300

    Atualização Geral

---
+++ menu.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-pills nav-justified">
+            <li role="presentation" class="active"><a href="?action=home">Home</a></li>            
+            <li role="presentation" ><a href="?action=listaOrgaoPagador">Orgão Pagador</a></li>
+            <li role="presentation"><a href="?action=listaUsuario">Usuários</a></li> 
+             <!--<li role="presentation"><a href="?action=listarStatusUsuario">Inserir Status Usuário</a></li>-->
+            <li role="presentation"><a href="?action=listaTipoPagamento">Cadastro Pagamento</a></li>
+            <li role="presentation"><a href="?action=listarTipoDespesa">Tipo Despesa</a></li>
+            <li role="presentation"><a href="?action=listarMesRef">Processar Calendário</a></li>            
+            <li role="presentation"><a href="?action=listarPagamentosIR">Imposto de Renda</a></li>
+            <!--<li role="presentation"><a href="?action=listarPagamentoMes">Inserir Pagamentos</a></li>-->
+            <!--<li role="presentation"><a href="?action=listarRelatorios">Relatórios</a></li>-->
+            <!--<li role="presentation"><a href="?action=listaStatusPagamento">Inserir Status Pagamentos</a></li>-->
+            <li role="presentation" ><a href="logout.php">Sair</a></li>
+</ul>
