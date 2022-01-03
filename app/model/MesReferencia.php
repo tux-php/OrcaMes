4a54216e51bc4712b074e39d558213dc3f4152e3
tux-php <fernandomcp23@gmail.com>
2022-01-03 20:42:23 -0300

    Correcao Ano 2022 Processamento

--- app/model/MesReferencia.php
+++ app/model/MesReferencia.php
@@ -6,8 +6,7 @@ class MesReferencia{
     public function listarMes() {
         try {
             $query = "SELECT * FROM mes_referencia WHERE d_e_l_e_t_e IS NULL
-                           AND id_mes_referencia BETWEEN 277 and 289";   
-                           //var_dump($query);die();                          
+                           AND id_mes_referencia BETWEEN 277 and 289";                              
             //Inserir uma paginação aqui
                     $conexao = Conexao::pegaConexao();
                     $rs = $conexao->query($query);
